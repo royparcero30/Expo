cb7ebe9c0c26bfc065381af72b259c204166f5b5
royparcero30 <rparcero@gbox.ncf.edu.ph>
2025-03-21 19:38:52 +0800

    HYJJJ

---
+++ app/UserDetail.tsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text, Button } from 'react-native';
+import { getUserByid } from './user/api';
+
+const UserDetail = ({ route }) => {
+  const { id } = route.params;
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    getUserByid(id).then(setUser);
+  }, [id]);
+
+  if (!user) {
+    return <Text>Loading...</Text>;
+  }
+
+  return (
+    <View>
+      <Text>User Detail</Text>
+      <Text>Username: {user.Username}</Text>
+    </View>
+  );
+};
+
+export default UserDetail;
